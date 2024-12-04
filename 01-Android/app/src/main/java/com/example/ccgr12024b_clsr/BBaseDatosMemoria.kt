d660914d5ef2d33e8b086485fcf34e5de537b94f
Cesar <cesarlui02@hotmail.com>
2024-12-04 02:14:04 -0500

    Igualado

---
+++ 01-Android/app/src/main/java/com/example/ccgr12024b_clsr/BBaseDatosMemoria.kt
@@ -0,0 +1,13 @@
+package com.example.ccgr12024b_clsr
+
+class BBaseDatosMemoria {
+    companion object {
+        val arregloBEntrenador = arrayListOf<BEntrenador>()
+
+        init {
+            arregloBEntrenador.add(BEntrenador(1, "Adrian", "a@a.com"))
+            arregloBEntrenador.add(BEntrenador(2, "Vicente", "b@b.com"))
+            arregloBEntrenador.add(BEntrenador(3, "Carolina", "c@c.com"))
+        }
+    }
+}
